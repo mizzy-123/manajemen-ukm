febc3ce7441602a6e6a87404f8bb0134d79b5906
colondum <decarabian21@gmail.com>
2024-01-27 11:43:38 +0700

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "test";
+const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 8080;
+const port = process.env.port || 8080;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
